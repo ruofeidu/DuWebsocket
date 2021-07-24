fd03b653f29216c9dd40379d2c9ec7747bdb0afd
Ruofei Du <xiaoxinghai@gmail.com>
2021-07-23 21:37:01 -0700

    Improved server.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const WebSocket = require('ws');
 var dict = {};
+var PORT = 8585;
+var PREFIX_GET = 'get ';
+var PREFIX_SET = 'set ';
+var RESULT_NULL = 'NULL';
+var RESULT_TRUE = 'True';
+var RESULT_FALSE = 'False';
 
-const wss = new WebSocket.Server({ port: 8585 },()=>{
-    console.log('server started')
+const wss = new WebSocket.Server({ port: PORT },()=>{
+  console.log('server started')
 });
 
 wss.on('connection', function connection(ws) {
@@ -14,33 +20,35 @@ wss.on('connection', function connection(ws) {
       }
 
       var cmd = data.substr(0, 4);
-      var msg = data.substr(5);
+      var msg = data.substr(4);
+      console.log(cmd, msg);
 
-      if (cmd == 'get ') {
+      if (cmd === PREFIX_GET) {
         if (msg in dict) {
           ws.send(msg + ':' + dict[msg]);
+          console.log('data sent \n %o', msg + ':' + dict[msg]);
           return;
         } else {
-          ws.send('NULL');
+          ws.send(RESULT_NULL);
           return;
         }
       } else
-      if (cmd == 'set ') {
-        var pos = cmd.indexOf(' ');
+      if (cmd == PREFIX_SET) {
+        var pos = msg.indexOf(' ');
         if (pos < 0) {
-          ws.send('False');
+          ws.send(RESULT_FALSE);
           return;
         }
 
         var k = msg.substr(0, pos);
         var v = msg.substr(pos + 1);
         dict[k] = v;
-        ws.send('True');
+        ws.send(RESULT_TRUE);
       }
     }
   })
 });
 
 wss.on('listening',()=>{
-   console.log('listening on 8585');
+   console.log('listening on ' + PORT);
 });
